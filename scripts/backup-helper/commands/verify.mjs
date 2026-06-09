d254171d271132c84233142e2e1e64adcb9a79df
bravonatalie <natalie.bravo@outlook.com>
2026-06-09 12:03:23 -0300

    feat(backup-helper): add verify command

---
+++ scripts/backup-helper/commands/verify.mjs
@@ -0,0 +1,9 @@
+/**
+ * @param {object} _args
+ * @param {string} _args.dir
+ * @param {object} _args.chain
+ * @param {number | undefined} _args.concurrency
+ */
+export async function runVerify(_args) {
+  throw new Error('verify: not implemented yet')
+}
